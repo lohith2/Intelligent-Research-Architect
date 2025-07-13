3ac5564451443ff7520ddf592c3574d0a74ac17d
lohith2 <lohitreddygms123@gmail.com>
2025-07-13 12:15:00 -0700

    feat(ui): build research interface with chat and source panels

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import { ResearchInterface } from './components/ResearchInterface';
+
+function App() {
+  return (
+    <div className="w-full h-screen">
+      <ResearchInterface />
+    </div>
+  );
+}
+
+export default App;
